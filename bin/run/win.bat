1aa90d65e99591a247d7f1dc0de9e9b4ffeb853f
Scott Monster Villarreal <dev@scottv.id>
2025-01-19 15:01:43 -0600

    create a windows run script

---
+++ bin/run/win.bat
@@ -0,0 +1,24 @@
+
+
+@REM This script should install choco, use choco to install lua
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install lua --version 5.1.5.52 -y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

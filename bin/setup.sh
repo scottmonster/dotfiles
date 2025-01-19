7861297bf547dbfa82c912ce6eb9354f1bf08d02
Scott Monster Villarreal <dev@scottv.id>
2025-01-19 15:31:09 -0600

    wrk

---
+++ bin/setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+
+# detirmine the package manager
+
+
+
+
+
+# we need to install ansible, python, and lua
+
+
+# for ansible
+# software-propertie-common
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

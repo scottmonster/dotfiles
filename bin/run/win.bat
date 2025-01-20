ecdba413ca694a06469bf6716413ddeb5ef79275
Scott Monster Villarreal <dev@scottv.id>
2025-01-19 18:38:57 -0600

    make win.bat echo until i get the chance/need to test it

--- bin/run/win.bat
+++ bin/run/win.bat
@@ -1,24 +1,15 @@
 
 
 @REM This script should install choco, use choco to install lua
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
-choco install lua --version 5.1.5.52 -y
 
 
+echo 'This needs to be tested before it's used an because my environment is a dumpster fire the networks aren't working for my windows VMs'
+echo 'This is what we WOULD do...'
 
+echo 'install choco with:'
+echo '@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"'
+echo @"%%SystemRoot%%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" ^&^& SET "PATH=%%PATH%%;%%ALLUSERSPROFILE%%\chocolatey\bin"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo 'install lua with:'
+echo '%ALLUSERSPROFILE%\chocolatey\bin\choco install lua --version 5.1.5.52 -y'
+echo %%ALLUSERSPROFILE%%\chocolatey\bin\choco install lua --version 5.1.5.52 -y
